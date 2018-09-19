4ba3d7efebc204feb9994556500c5d6e0b79f036
Justin Lee <justinlee.archer@gmail.com>
2018-09-19 15:25:40 +0800

    added interface contracts

--- app/src/main/java/com/justinlee/drawmatic/MainContract.java
+++ app/src/main/java/com/justinlee/drawmatic/MainContract.java
@@ -5,11 +5,52 @@ import com.justinlee.drawmatic.bases.BaseView;
 
 public interface MainContract {
     interface View extends BaseView<Presenter> {
+        // offline mode
+        void showOfflinePageUi();
 
+        void showOfflineGameSettingsPageUi();
+
+        // online mode
+        void showOnlinePageUi();
+
+        void showOnlineSearchPageUi();
+
+        void showOnlineRoomCreationPageUi();
+
+        void showOnlineGameSettingsPageUi();
+
+
+        // in-game pages
+        void showDrawingPageUi();
+
+        void showGuessingPageUi();
+
+        // settings
+        void showSettingsPageUi();
     }
 
     interface Presenter extends BasePresenter {
+        // offline mode
+        void transToOfflinePage();
 
+        void transToOfflineGameSettingsPage();
+
+        // online mode
+        void transToOnlinePage();
+
+        void transToOnlineSearchPage();
+
+        void transToOnlineRoomCreationPage();
+
+        void transToOnlineGameSettingsPage();
+
+        // in-game pages
+        void transToDrawingPage();
+
+        void transToGuessingPage();
+
+        // settings
+        void transToSettingsPage();
     }
 
 
